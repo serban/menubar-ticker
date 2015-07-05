07dc936aeaf662f695c6ad26c81a3ce3b7826999
Serban Giuroiu <giuroiu@gmail.com>
2015-07-05 11:18:07 -0700

    Covert OCUnit to XCTest to resolve deprecation
    From the Xcode 6.3.2 dialog:
    
      OCUnit is deprecated.
    
      The project “MenubarTicker” uses OCUnit for unit testing, which is deprecated.
      Choose “Convert to XCTest” to update this project to use XCTest for unit
      testing.

--- MenubarTickerTests/MenubarTickerTests.m
+++ MenubarTickerTests/MenubarTickerTests.m
@@ -26,7 +26,7 @@
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in MenubarTickerTests");
+    XCTFail(@"Unit tests are not implemented yet in MenubarTickerTests");
 }
 
 @end
